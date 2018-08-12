3ff2c0cf268f45d8708eb2ec298c25eb42885c60
tarik <oowest>
2018-08-12 21:46:07 +0300

    add imageTagsTest and LinksTest

---
+++ tests/Services/ImageTagsTest.php
@@ -0,0 +1,38 @@
+<?php declare( strict_types = 1 );
+
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * Class ImageTagsTest
+ */
+class ImageTagsTest extends TestCase
+{
+    public function testGetImgCount ()
+    {
+        $imgCount = \Services\Page\ImageTags::getImgCount(self::getTextWithImgTags());
+
+        static::assertEquals($imgCount, 2);
+
+        $imgCount = \Services\Page\ImageTags::getImgCount(self::getTextWithOutImgTags());
+
+        static::assertEquals($imgCount, 0);
+    }
+
+    /**
+     * @return string
+     */
+    public static function getTextWithImgTags ()
+    {
+        return '<img> <img>';
+    }
+
+    /**
+     * @return string
+     */
+    public static function getTextWithOutImgTags ()
+    {
+        return 'html';
+    }
+
+}
---
+++ tests/Services/LinksTest.php
@@ -0,0 +1,17 @@
+<?php declare( strict_types = 1 );
+
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * Class LinksTest
+ */
+class LinksTest extends TestCase
+{
+
+    public function testGetImgCount ()
+    {
+
+    }
+
+}
