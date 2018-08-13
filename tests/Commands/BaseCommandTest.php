4a0e846b4b5101585db4eb04ddea4de780ca86d2
tarik <oowest>
2018-08-13 13:33:23 +0300

    add BaseCommandTest

---
+++ tests/Commands/BaseCommandTest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+
+namespace Tests;
+
+use Commands\BaseCommand;
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * Class BaseCommandTest
+ * @package Tests
+ */
+class BaseCommandTest extends TestCase
+{
+    const GOOD_URL = 'https://google.com.ua';
+    const BAD_URL  = 'google.com.ua';
+    const SUCCESS  = 'Success';
+    const BAD_LINK = 'Bad link';
+
+    public function testRunSuccess()
+    {
+        $command = new BaseCommand();
+        $result  = $command->runCommand(self::GOOD_URL);
+
+        self::assertContains(self::SUCCESS, $result);
+    }
+
+    public function testRunFail()
+    {
+        $command = new BaseCommand();
+        $result  = $command->runCommand(self::BAD_URL);
+
+        self::assertContains(self::BAD_LINK, $result);
+    }
+}
