14d86be0610b11d6e2782260be41a05fd4bd5abc
tarik <oowest>
2018-08-13 13:28:49 +0300

    add LoaderTest

---
+++ tests/Services/LoaderTest.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types = 1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use Services\Page\Loader;
+use Services\Page\Page;
+
+/**
+ * Class LoaderTest
+ * @package Tests
+ */
+class LoaderTest extends TestCase
+{
+    const GOOD_URL = 'https://google.com.ua';
+    const BAD_URL  = 'google.com.ua';
+
+    /**
+     *
+     */
+    public function testLoad() : void
+    {
+        $loader = new Loader();
+        $result = $loader->load(self::GOOD_URL, 0);
+        self::assertNotEmpty($result);
+        self::assertInstanceOf(Page::class, $result);
+    }
+
+    /**
+     *
+     */
+    public function testLoadFalse() : void
+    {
+        $loader = new Loader();
+        $result = $loader->load(self::BAD_URL, 0);
+        self::assertNull($result);
+    }
+}
